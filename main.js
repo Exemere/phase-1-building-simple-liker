1668564bf38e7512fe57a4a86ec3c5e3e7bc836e
Livosero Esemere Nicolas <nesemere55@gmail.com>
2023-08-04 16:24:34 +0300

    update

--- main.js
+++ main.js
@@ -3,8 +3,41 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const heartIcons = document.querySelectorAll(".like-glyph");
+  const errorModal = document.getElementById("modal");
+  function showErrorModal(errorMessage) {
+    const modalMessage = document.getElementById("modal-message");
+    modalMessage.textContent = errorMessage;
 
+    errorModal.classList.remove("hidden");
+    setTimeout(() => {
+      errorModal.classList.add("hidden");
+    }, 3000);
+  }
 
+  function updateHeartStatus(heartIcon) {
+    if (heartIcon.textContent === EMPTY_HEART) {
+      heartIcon.textContent = FULL_HEART;
+      heartIcon.classList.add("activated-heart");
+    } else {
+      heartIcon.textContent = EMPTY_HEART;
+      heartIcon.classList.remove("activated-heart");
+    }
+  }
+
+  heartIcons.forEach((heartIcon) => {
+    heartIcon.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          updateHeartStatus(heartIcon);
+        })
+        .catch((error) => {
+          showErrorModal(error);
+        });
+    });
+  });
+});
 
 
 //------------------------------------------------------------------------------
